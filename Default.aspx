1ed7a263a17d12cfca335c3b52aac51e011365c8
Abhishek Mittal <mittalabhishek584@gmail.com>
2017-08-22 15:37:52 +0530

    Flash_1
    Under Construction

---
+++ Default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="projectstyle.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        
+        <asp:TextBox runat="server" ID="TeamName" MaxLength="20" Width="200px" Text="Team Name"></asp:TextBox>
+        <br /><br />
+        <asp:Label runat="server" ID="rolls">Roll Number</asp:Label>
+        &nbsp;&nbsp;
+        <asp:TextBox runat="server" ID="Member1" MaxLength="20" Width="200px"  Text="Roll Number: Member 1"></asp:TextBox>
+        <asp:TextBox runat="server" ID="Member2" MaxLength="20" Width="200px" Text="Roll Number: Member 2"></asp:TextBox>
+        <asp:TextBox runat="server" ID="Member3" MaxLength="20" Width="200px" Text="Roll Number: Member 3"></asp:TextBox>
+        <br /><br />
+        <asp:Label runat="server" ID="PTitle">Project Title</asp:Label>
+        &nbsp;&nbsp;
+        <asp:TextBox runat="server" ID="protitle" MaxLength="20" Width="200px" Text="Project Title" TextMode="SingleLine"></asp:TextBox>
+        <asp:TextBox runat="server" ID="prodes" MaxLength="20" Width="200px" Text="Breef Description about project." Height="50px"
+            TextMode="MultiLine"
+            ></asp:TextBox>
+        <asp:FileUpload runat="server" ID="project_file" />
+      
+         <asp:Button runat="server" ID="submit_data" UseSubmitBehavior="true" OnClick="Submit_Click" Text="Submit" />
+       
+        <div>
+            
+    </div>
+    </form>
+    <hr />
+   <div id="projectlist" runat="server" on>
+
+
+
+    </div>
+     <div  id="fl1" runat="server">
+
+
+
+      
+        </div>
+       <style type="text/css">
+             .flashcard {
+        margin-right: 15px;
+        padding: 10px;
+        border: 1px solid #9b1010;
+        height: 20%;
+        width: 20%;
+        float: left;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        color: whitesmoke;
+    }
+             .flashcard span{
+                 float:right;
+                 font-weight:bold;
+                 font-size:larger;
+                 color: white;
+                 cursor: pointer;
+             }
+         </style>
+    
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            
+            fl1.InnerHtml = "";
+            SqlConnection con1 = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con1.Open();
+            string oString = "Select * from projectlist";
+            SqlCommand oCmd = new SqlCommand(oString, con1);
+            SqlDataReader mdr = oCmd.ExecuteReader();
+
+            while (mdr.Read())
+            {
+                fl1.InnerHtml += "<div style='background-color: "+mdr[0]+ "' class='flashcard' runat='server'><span runat='server' onclick='dellEntry'>X</span><h3> Team : " + mdr[1] + " </h3><h4> Team Memebers: </h4><p> " + mdr[2] + " <br/> " + mdr[3] + " <br/> " + mdr[4] + " </p><h4> Project Name: " + mdr[5] + "</h4 > </div > ";
+            }
+
+            mdr.Close();
+            con1.Close();
+
+        }
+        catch (Exception p)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('wrong in here!')", true);
+        }
+        
+    }
+
+    void dellentry()
+    {
+        this.Parent.
+        SqlConnection con1 = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        con1.Open();
+        string oString = "Select * from projectlist";
+        SqlCommand oCmd = new SqlCommand(oString, con1);
+    }
+    
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+        var t = false;
+
+        //file upload: 
+
+        if (project_file.HasFile)
+        {
+            //file saviing into a local directory.
+            project_file.SaveAs("D:/" + project_file.FileName);
+
+        }
+        else
+        {
+            //no file uploaded
+        }
+
+        try
+        {
+            Random rnd = new Random();
+            var rndColor = String.Format("#{0:X6}", rnd.Next(0x1000000));
+
+
+
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            string oString = "INSERT INTO projectlist VALUES('" + rndColor + "','" + TeamName.Text + "','" + Member1.Text + "','" + Member2.Text + "','" + Member3.Text + "','" + protitle.Text + "','" + prodes.Text + "')";
+            SqlCommand oCmd = new SqlCommand(oString, con);
+            oCmd.ExecuteNonQuery();
+            con.Close();
+
+           
+            t = true;
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('something is wrong on not uneque :  ')", true);
+        }
+        finally
+        {
+            if(t)
+            {
+                Response.Redirect("Default.aspx?Load=Load");
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+                
+            }
+        }
+
+    }
+    
+
+    
+}
